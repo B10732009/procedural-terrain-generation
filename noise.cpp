b9f31e8d7e70cd9e9c7e0a04ffb29ffe1a4fb5b7
B10732009 <yuchen4667@yahoo.com.tw>
2023-05-25 14:51:26 +0800

    Add 3D perlin noise

--- noise.cpp
+++ noise.cpp
@@ -6,6 +6,7 @@ namespace py = pybind11;
 
 #include "noise/noise1d.hpp"
 #include "noise/noise2d.hpp"
+#include "noise/noise3d.hpp"
 
 PYBIND11_MODULE(noise, m)
 {
@@ -26,4 +27,16 @@ PYBIND11_MODULE(noise, m)
       .def_property_readonly("data",
                              [](Noise2D const &n) { return std::vector<double>(n.data(), n.data() + n.x() * n.y()); })
       .def("__getitem__", [](Noise2D const &n, std::vector<std::size_t> idx) { return n(idx[0], idx[1]); });
+
+  py::class_<Noise3D>(m, "Noise3D")
+      .def(py::init<std::size_t, std::size_t, std::size_t, std::size_t>())
+      .def(py::init<std::size_t, std::size_t, std::size_t, std::size_t, double, int, double, double>())
+      .def_property_readonly("seed", [](Noise3D const &n) { return n.seed(); })
+      .def_property_readonly("x", [](Noise3D const &n) { return n.x(); })
+      .def_property_readonly("y", [](Noise3D const &n) { return n.y(); })
+      .def_property_readonly("z", [](Noise3D const &n) { return n.z(); })
+      //   .def_property_readonly("data",
+      //                          [](Noise3D const &n) { return std::vector<double>(n.data(), n.data() + n.x() * n.y());
+      //                          })
+      .def("__getitem__", [](Noise3D const &n, std::vector<std::size_t> idx) { return n(idx[0], idx[1], idx[2]); });
 }
